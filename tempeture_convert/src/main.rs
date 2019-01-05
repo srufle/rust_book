f02dfb4d03e98530c38a76311bd06cfa0215d09c
Stephen Rufle <srufle@gmail.com>
2019-01-04 23:17:58 -0700

    Addin tempetue con version project

---
+++ tempeture_convert/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let f = 32.0;
+    let ans_c = (32.0 - f) * (5.0 / 9.0);
+    println!("Temp in C is {}", ans_c);
+
+    let c = 100.0;
+    let ans_f = c * (9.0 / 5.0) + 32.0;
+    println!("Temp in F is {}", ans_f);
+}
