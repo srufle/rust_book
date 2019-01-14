c9ea4214c8e46b1a0d22f31f561aada111b445d7
Stephen Rufle <srufle@gmail.com>
2019-01-14 07:33:02 -0700

    Adding thread from chapter 16

---
+++ thread_work/src/main.rs
@@ -0,0 +1,37 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    join_thread();
+    simple_thread();
+}
+fn join_thread() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("JOIN: Hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    // Putting the join here makes it look serial
+    // handle.join().unwrap();
+    for i in 1..5 {
+        println!("JOIN: Hi number {} from main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    // Putting the join here interleaves the messages and
+    // waits for the spawned thread to finish.
+    handle.join().unwrap();
+}
+fn simple_thread() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("SIMPLE: Hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("SIMPLE: Hi number {} from main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
