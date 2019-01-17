32c50a46f5ebd1de75150a586361a46222db37f1
Stephen Rufle <srufle@gmail.com>
2019-01-16 20:43:48 -0700

    Added version 2 - Encoding States and Behavior as Types from chapter 17

--- blog_work/src/lib.rs
+++ blog_work/src/lib.rs
@@ -1,12 +1,12 @@
-pub struct Post {
-    state: Option<Box<State>>,
+pub struct PostV1 {
+    state: Option<Box<StateV1>>,
     content: String,
 }
 
-impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+impl PostV1 {
+    pub fn new() -> PostV1 {
+        PostV1 {
+            state: Some(Box::new(DraftV1 {})),
             content: String::new(),
         }
     }
@@ -19,7 +19,7 @@ impl Post {
         self.state.as_ref().unwrap().content(&self)
     }
 
-    // If there were more of these type methods 
+    // If there were more of these type methods
     // looking into using macros, might be useful
     pub fn request_review(&mut self) {
         if let Some(s) = self.state.take() {
@@ -33,47 +33,91 @@ impl Post {
     }
 }
 
-trait State {
-    fn request_review(self: Box<Self>) -> Box<State>;
-    fn approve(self: Box<Self>) -> Box<State>;
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
+trait StateV1 {
+    fn request_review(self: Box<Self>) -> Box<StateV1>;
+    fn approve(self: Box<Self>) -> Box<StateV1>;
+    fn content<'a>(&self, post: &'a PostV1) -> &'a str {
         ""
     }
 }
 
-struct Draft {}
+struct DraftV1 {}
 
-impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<State> {
-        Box::new(PendingReview {})
+impl StateV1 for DraftV1 {
+    fn request_review(self: Box<Self>) -> Box<StateV1> {
+        Box::new(PendingReviewV1 {})
     }
-    fn approve(self: Box<Self>) -> Box<State> {
+    fn approve(self: Box<Self>) -> Box<StateV1> {
         self
     }
     // could add content method where we decorate with "DRAFT"
 }
 
-struct PendingReview {}
+struct PendingReviewV1 {}
 
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<State> {
+impl StateV1 for PendingReviewV1 {
+    fn request_review(self: Box<Self>) -> Box<StateV1> {
         self
     }
-    fn approve(self: Box<Self>) -> Box<State> {
-        Box::new(Published {})
+    fn approve(self: Box<Self>) -> Box<StateV1> {
+        Box::new(PublishedV1 {})
     }
 }
 
-struct Published {}
+struct PublishedV1 {}
 
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<State> {
+impl StateV1 for PublishedV1 {
+    fn request_review(self: Box<Self>) -> Box<StateV1> {
         self
     }
-    fn approve(self: Box<Self>) -> Box<State> {
+    fn approve(self: Box<Self>) -> Box<StateV1> {
         self
     }
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
+    fn content<'a>(&self, post: &'a PostV1) -> &'a str {
         &post.content
+    }
+}
+
+// Encoding States and Behavior as Types
+pub struct DraftPostV2 {
+    content: String,
+}
+
+pub struct PostV2 {
+    content: String,
+}
+
+pub struct PendingReviewPostV2 {
+    content: String,
+}
+
+impl PostV2 {
+    pub fn new() -> DraftPostV2 {
+        DraftPostV2 {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPostV2 {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+    pub fn request_review(self) -> PendingReviewPostV2 {
+        PendingReviewPostV2 {
+            content: self.content,
+        }
+    }
+}
+
+impl PendingReviewPostV2 {
+    pub fn approve(self) -> PostV2 {
+        PostV2 {
+            content: self.content,
+        }
     }
 }
--- blog_work/src/main.rs
+++ blog_work/src/main.rs
@@ -1,8 +1,14 @@
 extern crate blog_work;
-use blog_work::Post;
+use blog_work::PostV1;
+use blog_work::PostV2;
 
 fn main() {
-    let mut post = Post::new();
+    version1();
+    version2();
+}
+
+fn version1() {
+    let mut post = PostV1::new();
 
     post.add_text("I ate a salad for lunch today");
     assert_eq!("", post.content());
@@ -12,4 +18,16 @@ fn main() {
 
     post.approve();
     assert_eq!("I ate a salad for lunch today", post.content());
+}
+
+fn version2() {
+    let mut post = PostV2::new();
+
+    post.add_text("I ate burgers for lunch today!");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate burgers for lunch today!", post.content());
 }
